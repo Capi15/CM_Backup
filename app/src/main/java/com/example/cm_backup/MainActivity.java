87ede69fd161a96e8c581aa22bf59a8908de0e96
Capi15 <capicapi1515@gmail.com>
2020-03-18 19:09:23 +0000

    basic local list of strings implemented

--- app/src/main/java/com/example/cm_backup/MainActivity.java
+++ app/src/main/java/com/example/cm_backup/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.cm_backup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fillLista();
+    }
+
+
+    private void fillLista(){
+        ArrayList<String> arrayItems = new ArrayList<>();
+        arrayItems.add("Nota1");
+        arrayItems.add("Nota2");
+        arrayItems.add("Nota3");
+
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrayItems);
+        ((ListView) findViewById(R.id.id_lista)).setAdapter(itemsAdapter);
     }
 }
---
+++ app/src/main/java/com/example/cm_backup/NotasActivity.java
@@ -0,0 +1,21 @@
+package com.example.cm_backup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+
+public class NotasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notas);
+
+
+    }
+
+
+}
