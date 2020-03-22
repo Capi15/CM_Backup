a8c689a97aaab69aa0e8b0bea58b5b80257c8e7a
Capi15 <capicapi1515@gmail.com>
2020-03-22 11:08:40 +0000

    basic local addin context menu

--- app/src/main/java/com/example/cm_backup/ativities/MainActivity.java
+++ app/src/main/java/com/example/cm_backup/ativities/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.cm_backup.ativities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.cm_backup.R;
 import com.example.cm_backup.adapters.CustomArrayAdapter;
@@ -14,6 +20,7 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ArrayList<Nota> arrayNota;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,5 +37,26 @@ public class MainActivity extends AppCompatActivity {
 
         CustomArrayAdapter itemsAdapter = new CustomArrayAdapter(this, arrayItems);
         ((ListView) findViewById(R.id.id_lista)).setAdapter(itemsAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.context_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        int index = info.position;
+        Nota nota = arrayNota.get(index);
+
+        switch (item.getItemId()){
+            case R.id.adicionar:
+                Toast.makeText(this, "adicionado", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return super.onContextItemSelected(item);
     }
 }
