6832d2fde3b102eb696b96f1324bba4dfcd9f4c1
Capi15 <capicapi1515@gmail.com>
2020-03-25 19:06:26 +0000

    created the notaRoom repository and nota repository

---
+++ app/src/main/java/NotaRepository.java
@@ -0,0 +1,43 @@
+import android.app.Application;
+import android.os.AsyncTask;
+
+import androidx.lifecycle.LiveData;
+
+import com.example.cm_backup.entities.Nota;
+
+import java.util.List;
+
+public class NotaRepository {
+    private NotaDao notaDao;
+    private LiveData<List<Nota>> allNotas;
+
+    public NotaRepository(Application application){
+        NotaRoomDatabase db = NotaRoomDatabase.getDatabase(application);
+        notaDao = db.notaDao();
+        allNotas = notaDao.getAllNotas();
+
+    }
+
+    LiveData<List<Nota>> getAllNotas(){
+        return allNotas;
+    }
+
+    public void insert (Nota nota){
+        new insertAsyncTask(notaDao).execute(nota);
+    }
+
+    private static class insertAsyncTask extends AsyncTask<Nota, Void, Void> {
+
+        private NotaDao mAsyncTaskDao;
+
+        insertAsyncTask(NotaDao notaDao) {
+            mAsyncTaskDao = notaDao;
+        }
+
+        @Override
+        protected Void doInBackground(final Nota... params) {
+            mAsyncTaskDao.insert(params[0]);
+            return null;
+        }
+    }
+}
