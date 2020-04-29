76397c6e8898a215f1c5278915193318a23c1541
Capi15 <capicapi1515@gmail.com>
2020-04-29 14:08:16 +0100

    db connections

--- app/src/main/java/com/example/cm_backup/ativities/LoginActivity.java
+++ app/src/main/java/com/example/cm_backup/ativities/LoginActivity.java
@@ -5,8 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.example.cm_backup.R;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
 
